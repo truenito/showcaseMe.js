884817d68e1ab08057aaf9b30f2909dfe57f41ed
Gabriel González Mañaná <Gabriel@Estilopanda.com>
2014-06-19 12:09:41 -0400

    No need for sliderWrapper anymore, just initialise on wrapper div.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 // Initialize showcaseMe
 $(document).ready(function () {
-  $('.horizontal-showcase').showcaseMe({
-    sliderWrapper: "#horizontal-widget",
     animationInNegative: "animated bounceInLeft",
     animationOutNegative: "animated bounceOutLeft",
     animationInPositive: "animated bounceInRight",
     animationOutPositive: "animated bounceOutRight",
+  $('#horizontal-widget').showcaseMe({
     nextBtn: ".horizontal-slider-rightarrow",
     prevBtn: ".horizontal-slider-leftarrow",
     desaturateClass: "desaturate",
